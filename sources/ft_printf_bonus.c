2eebaab842acdef7388baa49982dd5329adf5b71
Memthos <perrinetrebi.m@gmail.com>
2026-03-03 23:48:40 +0100

    start flags parsing

--- sources/ft_printf_bonus.c
+++ sources/ft_printf_bonus.c
@@ -6,29 +6,42 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 00:34:05 by mperrine          #+#    #+#             */
-/*   Updated: 2026/03/03 23:36:06 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/03/03 23:48:40 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../headers/ft_printf_bonus.h"
+#include "../headers/ft_printf.h"
 
-static int	ft_parse_specifiers(const char c, t_ft_printf *data)
+static int	is_flag(char c)
 {
-	if (c == 'c')
+	if (c == '-' || c == '0' || c == '.' || c == '#' || c == ' ' || c == '+')
+		return (1);
+	return (0);
+}
+
+static void	parse_flags(char *format, t_ft_printf *data, int *i)
+{
+}
+
+static int	ft_parse_specifiers(char *s, t_ft_printf *data, int *i)
+{
+	while (s[*i] && is_flag(s[*i]))
+		parse_flags(s, data, i++);
+	if (s[*i] == 'c')
 		ft_putchar(va_arg(*(data->args), int), data);
-	else if (c == 's')
+	else if (s[*i] == 's')
 		ft_putstr(va_arg(*(data->args), char *), data);
-	else if (c == 'p')
+	else if (s[*i] == 'p')
 		ft_putpointer((unsigned long int)va_arg(*(data->args), void *), data);
-	else if (c == 'd' || c == 'i')
+	else if (s[*i] == 'd' || s[*i] == 'i')
 		ft_putnbr(va_arg(*(data->args), int), data);
-	else if (c == 'u')
+	else if (s[*i] == 'u')
 		ft_putnbr_unsigned(va_arg(*(data->args), unsigned int), data);
-	else if (c == 'x')
+	else if (s[*i] == 'x')
 		ft_puthexa((unsigned int)va_arg(*(data->args), int), 0, data);
-	else if (c == 'X')
+	else if (s[*i] == 'X')
 		ft_puthexa((unsigned int)va_arg(*(data->args), int), 1, data);
-	else if (c == '%')
+	else if (s[*i] == '%')
 		ft_putchar('%', data);
 	else
 		return (1);
@@ -50,7 +63,8 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			if (!ft_parse_specifiers(format[++i], &data))
+			i++;
+			if (!ft_parse_specifiers(format, &data, &i))
 				i++;
 		}
 		else
